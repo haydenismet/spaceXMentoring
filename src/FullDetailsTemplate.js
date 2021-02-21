ff00920765a413b79a0b255c7710d4a738b65f94
joshua-tree-ctrl <55064706+joshua-tree-ctrl@users.noreply.github.com>
2021-02-21 13:04:51 +0000

    full details formatting

--- src/FullDetailsTemplate.js
+++ src/FullDetailsTemplate.js
@@ -3,20 +3,24 @@ function FullDetailsTemplate(props) {
 console.log(props.selectedItem);
 
   return (
+      <>
+     
     <section className="full-details-view">
+    <div className="detail-list">detail-list</div>
        {props.spaceData.launches.filter(selectedMission => selectedMission.id === props.selectedItem).map(filteredSelection => (
-    <>
+   
     <ul className="selected-mission">
       {console.log(filteredSelection)}
       <li className="mission-name" key={filteredSelection.mission_name}>{filteredSelection.mission_name}</li>
       <li className="mission-year" key={filteredSelection.launch_year}>{filteredSelection.launch_year}</li>
-    
+      <li>{filteredSelection.details}</li>
     </ul>
-      <div>{filteredSelection.details}</div>
-      </>
+   
+    
   ))}
      
   </section>
+  </>
   );
 }
 export default FullDetailsTemplate;
